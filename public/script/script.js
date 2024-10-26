ea85f487d48a3f59bee5c71307dcac0c4cd2f360
Pedro vinicius V7 <163039257+PedroVinicins@users.noreply.github.com>
2024-10-26 19:59:27 -0300

    Store N/

--- public/script/script.js
+++ public/script/script.js
@@ -3,15 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const saveButton = document.querySelector('.btn-nav2:nth-child(1)');
     const reiniciaButton = document.querySelector('.btn-nav2:nth-child(2)');
     const upgradeClickButton = document.getElementById("upgradeClick");
-    const upgradeBananaButton = document.getElementById("upgradeBanana");
+    const superClickbutton = document.getElementById("superClick");
     const vendasButtons = document.querySelectorAll(".vendas .btn-");
 
     let banana = 0;
     let points = 0;
     let autoClickEnabled = false;
     let clickMultiplier = 1;
-
-    // Atualiza a pontuação na tela
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     function pontuacao() {
         document.querySelector('.btn-nav:nth-child(1)').innerText = `🍌: ${banana}`;
     }
@@ -32,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Upgrade Auto Click
+    // Upgrade Auto Click   // Upgrade Super Click
     upgradeClickButton.addEventListener("click", function () {
         if (banana >= 10 && !autoClickEnabled) {
             banana -= 10;
@@ -41,14 +40,13 @@ document.addEventListener('DOMContentLoaded', () => {
             setInterval(() => {
                 banana += 1;
                 pontuacao();
-            }, 1000); // Adiciona 1 ponto automaticamente a cada segundo
+            }, 1000); // 1 pontos a cada segundo
         } else {
             alert("Pontos insuficientes ou upgrade já comprado!");
         }
     });
-
-    // Upgrade Super Click
-    upgradeBananaButton.addEventListener("click", function () {
+  
+    superClickbutton.addEventListener("click", function () {
         if (banana >= 100) {
             banana -= 100;
             clickMultiplier = 2; // Dobra os pontos por clique
@@ -57,20 +55,6 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             alert("Pontos insuficientes!");
         }
-    });
-
-    // Evento de compra para itens da seção de vendas
-    vendasButtons.forEach(button => {
-        button.addEventListener("click", function () {
-            const itemPrice = parseInt(this.innerText.split('- ')[1]);
-            if (banana >= itemPrice) {
-                banana -= itemPrice;
-                pontuacao();
-                alert(`Você comprou ${this.innerText.split(' - ')[0]}!`);
-            } else {
-                alert("Pontos insuficientes!");
-            }
-        });
     });
 
     // Salvar progresso
